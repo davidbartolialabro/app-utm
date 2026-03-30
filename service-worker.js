9c5cbe8ba5775b1f8e78c1b4dfcc2e70b42f8724
davidbartolialabro <david.bartoli.alabro@gmail.com>
2026-03-30 20:34:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "utm-app-v1";
+const CACHE_NAME = "utm-app-v2";
 
 const urlsToCache = [
   "./",
@@ -9,16 +9,14 @@ const urlsToCache = [
   "https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.9.0/proj4.js"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
